f1591444ff2b2a834249bd76bfc4da7cb0654e10
James Lyne <jim+github@not-null.co.uk>
2021-06-20 03:00:03 +0100

    Fix empty chunk sending on 1.17

--- versions/version_1_17.py
+++ versions/version_1_17.py
@@ -1,4 +1,5 @@
-from quarry.types.nbt import TagInt
+from quarry.types.chunk import PackedArray
+from quarry.types.nbt import TagInt, TagLongArray, TagCompound
 
 from versions import Version_1_16_2
 from waitingserver import Protocol
@@ -34,3 +35,24 @@ class Version_1_17(Version_1_16_2):
                                           int(spawn.get('y')),
                                           int(spawn.get('z'))),
                                       self.protocol.buff_type.pack("ib", 0, False))
+
+    def send_reset_world(self):
+        data = [
+            self.protocol.buff_type.pack_varint(0),
+            b'',
+            self.protocol.buff_type.pack_nbt(
+                TagRoot({'': TagCompound({"MOTION_BLOCKING": TagLongArray(PackedArray.empty_height())})})),
+            self.protocol.buff_type.pack_varint(1024),
+        ]
+
+        for i in range(1024):
+            data.append(self.protocol.buff_type.pack_varint(127))
+
+        data.append(self.protocol.buff_type.pack_varint(0))
+        data.append(b'')
+        data.append(self.protocol.buff_type.pack_varint(0))
+        data.append(b'')
+
+        for x in range(-8, 8):
+            for y in range(-8, 8):
+                self.protocol.send_packet("chunk_data", self.protocol.buff_type.pack("ii", x, y), *data)
