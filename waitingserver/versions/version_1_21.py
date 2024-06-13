c54fc60a47c6ec07dcee057adbfbc05a8c88ee0c
James Lyne <jim+github@not-null.co.uk>
2024-06-13 20:44:41 +0100

    1.21

---
+++ waitingserver/versions/version_1_21.py
@@ -0,0 +1,10 @@
+from waitingserver.protocol import Protocol
+from waitingserver.versions import Version_1_20_5
+
+
+class Version_1_21(Version_1_20_5):
+    protocol_version = 767
+    tag_format = '1.21'
+
+    def __init__(self, protocol: Protocol, bedrock: False):
+        super(Version_1_21, self).__init__(protocol, bedrock)
