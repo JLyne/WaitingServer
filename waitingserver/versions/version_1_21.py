1d9ddd5fd960a87259560da22babf85820258a18
James Lyne <jim+github@not-null.co.uk>
2024-10-26 22:49:42 +0100

    Add 1.21 packets

--- waitingserver/versions/version_1_21.py
+++ waitingserver/versions/version_1_21.py
@@ -4,6 +4,7 @@ from waitingserver.versions import Version_1_20_5
 
 class Version_1_21(Version_1_20_5):
     protocol_version = 767
+    chunk_format = '1.21'
 
     def __init__(self, protocol: Protocol, bedrock: False):
         super(Version_1_21, self).__init__(protocol, bedrock)
