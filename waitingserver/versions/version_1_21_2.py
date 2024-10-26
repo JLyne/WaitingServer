908e79b6a03c3dc6342698f5d03ee45646bb1c53
James Lyne <jim+github@not-null.co.uk>
2024-10-26 23:12:28 +0100

    1.21.2 fixes

--- waitingserver/versions/version_1_21_2.py
+++ waitingserver/versions/version_1_21_2.py
@@ -28,13 +28,13 @@ class Version_1_21_2(Version_1_21):
                                   self.protocol.buff_type.pack_varint(7),
                                   self.protocol.buff_type.pack_varint(0),
                                   self.protocol.buff_type.pack("???", False, True, False),
-                                  self.protocol.buff_type.pack_varint(id),  # Now varint
+                                  self.protocol.buff_type.pack_varint(id),
                                   self.protocol.buff_type.pack_string("rtgame:waiting"),
                                   self.protocol.buff_type.pack("qBb??", 0, 1, 1, False, False),
                                   self.protocol.buff_type.pack("?", False),
                                   self.protocol.buff_type.pack_varint(0),
-                                  self.protocol.buff_type.pack_varint(64),
-                                  self.protocol.buff_type.pack("?", False)) # Sea level
+                                  self.protocol.buff_type.pack_varint(64), # Sea level
+                                  self.protocol.buff_type.pack("?", False))
 
     def send_spawn(self, effects=False):
         spawn = self.current_world.spawn
@@ -44,11 +44,12 @@ class Version_1_21_2(Version_1_21):
                                                                int(spawn.get('z'))))
 
         self.protocol.send_packet("player_position",
-                                  self.protocol.buff_type.pack_varint(0),
+                                  self.protocol.buff_type.pack_varint(0), # Move to front
                                   self.protocol.buff_type.pack("ddddddffi",
                                                                spawn.get('x') + 0.5, spawn.get('y'), spawn.get('z') + 0.5,
-                                                               0, 0, 0,
-                                                               spawn.get('yaw'), spawn.get('pitch'), 0))
+                                                               0, 0, 0, # Add second vector for movement
+                                                               spawn.get('yaw'), spawn.get('pitch'),
+                                                               0)) # Bitset instead of boolean
 
         if effects is True:
             self.send_spawn_effect()
@@ -59,27 +60,14 @@ class Version_1_21_2(Version_1_21):
         # Time of day
         self.protocol.send_packet('set_time',
                                   self.protocol.buff_type.pack("Qq?", 0, self.current_world.time,
-                                                               not self.current_world.cycle))
-
-    def send_spawn_effect(self):
-        spawn = self.current_world.spawn
-
-        self.protocol.send_packet("level_event",
-                                  self.protocol.buff_type.pack("i", 2003),
-                                  self.protocol.buff_type.pack_position(
-                                      int(spawn.get('x')),
-                                      int(spawn.get('y')),
-                                      int(spawn.get('z'))),
-                                  self.protocol.buff_type.pack("ib", 0, False))
-
-        self.send_global_sound("minecraft:entity.enderman.teleport", 6)
+                                                               not self.current_world.cycle)) # Boolean for fixed time instead of negative time value
 
     def send_respawn(self):
         dimension_registry = self.protocol.data_packs.get_registry(NamespacedKey.minecraft('dimension_type'))
         id = list(dimension_registry).index(self.current_world.dimension)
 
         self.protocol.send_packet("respawn",
-                                  self.protocol.buff_type.pack_varint(id), # Now varint
+                                  self.protocol.buff_type.pack_varint(id),
                                   self.protocol.buff_type.pack_string("rtgame:reset"),
                                   self.protocol.buff_type.pack("qBB", 0, 1, 1),
                                   self.protocol.buff_type.pack("???", False, False, False),
@@ -88,7 +76,7 @@ class Version_1_21_2(Version_1_21):
                                   self.protocol.buff_type.pack_varint(64)) # Sea Level
 
         self.protocol.send_packet("respawn",
-                                  self.protocol.buff_type.pack_varint(id), # Now varint
+                                  self.protocol.buff_type.pack_varint(id),
                                   self.protocol.buff_type.pack_string("rtgame:waiting"),
                                   self.protocol.buff_type.pack("qBB", 0, 1, 1),
                                   self.protocol.buff_type.pack("???", False, False, False),
