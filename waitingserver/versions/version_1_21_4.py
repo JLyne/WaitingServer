6bb79b74b0637f8b098bf6fcd56e762b2137c86e
James Lyne <jim+github@not-null.co.uk>
2024-12-03 18:47:21 +0000

    1.21.4

---
+++ waitingserver/versions/version_1_21_4.py
@@ -0,0 +1,14 @@
+from waitingserver.protocol import Protocol
+from waitingserver.versions import Version_1_21_2
+
+
+class Version_1_21_4(Version_1_21_2):
+    protocol_version = 769
+    chunk_format = '1.21.4'
+
+    hologram_entity_id = 124  # Text display
+    map_entity_id = 57  # Glow item frame
+    map_item_id = 1031  # Filled map
+
+    def __init__(self, protocol: Protocol, bedrock: False):
+        super(Version_1_21_2, self).__init__(protocol, bedrock)
